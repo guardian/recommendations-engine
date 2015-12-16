45c1797da034b0d29c2d31b05dac68fbecbf9f8e
David Furey <david.furey@guardian.co.uk>
2015-12-16 15:44:08 +0000

    Universal recommendation engine

---
+++ provisioning/prediction-io-dist/mobile-prediction-io/PROD/standalone/create-engine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /opt/PredictionIO-0.9.5/
+
+mkdir engines
+
+cd engines
+
+yes | pio template get PredictionIO/template-scala-parallel-universal-recommendation \
+    --version v0.2.3 \
+    --name 'TheGuardianNews&Media' \
+    --package 'com.gu' \
+    --email 'mobile.server.side@theguardian.com' \
+    MyGuardianTestEngine-1
+
+pio app new MyGuardianTestEngine-1
+
+cp /root/files/engine.json MyGuardianTestEngine-1/
+
+pio build
+# pio train
+# pio deploy
