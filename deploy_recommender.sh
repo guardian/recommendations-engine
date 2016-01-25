17a3a591bf7839843cf6be20e5b41882b879824c
David Furey <david.furey@guardian.co.uk>
2016-01-25 11:23:03 +0000

    Update recommender directory structure in preparation for adding app

--- deploy_recommender.sh
+++ deploy_recommender.sh
@@ -3,7 +3,7 @@
 rm -rf target
 mkdir target
 mkdir target/files
-cp recommender/* target/files/
+cp recommender/files/* target/files/
 cd target
 tar -czf package.tar.gz files
 aws s3 cp package.tar.gz s3://prediction-io-dist/mobile-prediction-io/PROD/recommender/package.tar.gz
