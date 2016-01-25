e42dc12c15016ea38a541495e89c197b95ae4a72
David Furey <david.furey@guardian.co.uk>
2016-01-25 11:23:41 +0000

    Remove verbose tar flag and update to spark 1.5.2

--- standalone/setup.sh
+++ standalone/setup.sh
@@ -3,18 +3,18 @@
 cd /opt/
 
 wget https://d8k1yxp8elc6b.cloudfront.net/PredictionIO-0.9.5.tar.gz
-tar zxvf PredictionIO-0.9.5.tar.gz
+tar zxf PredictionIO-0.9.5.tar.gz
 
 mkdir PredictionIO-0.9.5/vendors
 
-wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.1-bin-hadoop2.6.tgz
-tar zxvfC spark-1.5.1-bin-hadoop2.6.tgz PredictionIO-0.9.5/vendors
+wget http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz
+tar zxfC spark-1.5.2-bin-hadoop2.6.tgz PredictionIO-0.9.5/vendors
 
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz
-tar zxvfC elasticsearch-1.4.4.tar.gz PredictionIO-0.9.5/vendors
+tar zxfC elasticsearch-1.4.4.tar.gz PredictionIO-0.9.5/vendors
 
 wget http://mirrors.ukfast.co.uk/sites/ftp.apache.org/hbase/hbase-1.0.2/hbase-1.0.2-bin.tar.gz
-tar zxvfC hbase-1.0.2-bin.tar.gz PredictionIO-0.9.5/vendors
+tar zxfC hbase-1.0.2-bin.tar.gz PredictionIO-0.9.5/vendors
 
 cp /root/files/elasticsearch.yml PredictionIO-0.9.5/vendors/elasticsearch-1.4.4/config/
 cp /root/files/hbase-env.sh PredictionIO-0.9.5/vendors/hbase-1.0.2/conf/
