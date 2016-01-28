8244f7a352cddb2320cd0cd4c8ce7d463308b72b
agulowaty <artur.gulowaty@guardian.co.uk>
2016-01-28 11:50:15 +0000

    - adding bootstrap files for RR/CF deployment - sbt-tc scripts added - pio distribution setup to top of repo structure, renamings, cleanup

---
+++ pio-dist/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+instanceid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone |sed 's/.$//'`
+
+stagetag=`aws ec2 describe-tags --filters "Name=resource-id,Values=$instanceid" "Name=resource-type,Values=instance" "Name=key,Values=Stage" --region $region | grep -oP "(?<=\"Value\": \")[^\"]+"`
+
+HADOOP_MASTER=namenode.hadoop.recommendations.$stagetag.internal.
+ELASTICSEARCH_HOSTS=nodes.elasticsearch.recommendations.$stagetag.internal.
+PIO_DIST=/root/pio-dist
+
+cd /opt/
+
+wget https://d8k1yxp8elc6b.cloudfront.net/PredictionIO-0.9.5.tar.gz
+tar zxf PredictionIO-0.9.5.tar.gz
+
+mkdir /opt/PredictionIO-0.9.5/engines
+tar zxf /root/engine.tar.gz --directory /opt/PredictionIO-0.9.5/engines/
+
+mkdir -p PredictionIO-0.9.5/conf/hbase/
+
+cp ${PIO_DIST}/hbase-env.sh PredictionIO-0.9.5/conf/hbase/
+sed 's/<HADOOP-MASTER>/'$HADOOP_MASTER'/g' ${PIO_DIST}/hbase-site.xml.template > PredictionIO-0.9.5/conf/hbase/hbase-site.xml
+
+sed 's/<ELASTICSEARCH-HOSTS>/'${ELASTICSEARCH_HOSTS}'/g' ${PIO_DIST}/pio-env.sh.template > PredictionIO-0.9.5/conf/pio-env.sh
+
+cp ${PIO_DIST}/java_home.sh /etc/profile.d/
+cp ${PIO_DIST}/pio-eventserver.conf /etc/init/
+cp ${PIO_DIST}/pio-deploy.conf /etc/init/
+
+chown ubuntu.ubuntu -R PredictionIO-0.9.5
--- recommender/build.sbt
+++ recommender/build.sbt
@@ -6,25 +6,21 @@ scalaVersion := "2.11.6"
 lazy val root = (project in file("."))
   .enablePlugins(PlayScala, RiffRaffArtifact, JavaAppPackaging)
   .settings(
+    mappings in Universal ++= (baseDirectory.value / "resources" ***).get pair relativeTo(baseDirectory.value),
+    riffRaffPackageType := (packageZipTarball in config("universal")).value,
+    libraryDependencies ++= Seq(
+      "org.scalaz" %% "scalaz-core" % "7.0.6",
+      "com.gu" %% "configuration" % "4.0",
+      ws,
+      json,
+      "org.mockito" % "mockito-all" % "1.9.0" % "test",
+      "org.scalacheck" %% "scalacheck" % "1.12.2" % "test",
+      "org.specs2" %% "specs2" % "2.3.12" % "test"
+    ),
     routesImport += "binders._",
     routesImport += "models._",
     routesImport += "org.joda.time.DateTime"
   )
 
-mappings in Universal ++= (baseDirectory.value / "resources" *** ).get pair relativeTo(baseDirectory.value)
-
-riffRaffPackageType := (packageZipTarball in config("universal")).value
-
 addCommandAlias("dist", ";riffRaffArtifact")
-
-libraryDependencies ++= Seq(
-  "org.scalaz" %% "scalaz-core" % "7.0.6",
-  "com.gu" %% "configuration" % "4.0",
-  ws,
-  json,
-  // Test dependencies
-  "org.mockito" % "mockito-all" % "1.9.0" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.12.2" % "test",
-  "org.specs2" %% "specs2" % "2.3.12" % "test"
-)
 
