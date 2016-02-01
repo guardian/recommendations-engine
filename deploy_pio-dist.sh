f4f873a43c76cbc0c783353cbc403bdab72d8e18
agulowaty <artur.gulowaty@guardian.co.uk>
2016-02-01 16:14:12 +0000

    fixed S3 bucket name for recommendations

--- deploy_pio-dist.sh
+++ deploy_pio-dist.sh
@@ -9,5 +9,5 @@ cp pio-dist/* target/pio-dist/
 
 cd target
 tar -czf pio-dist.tar.gz pio-dist
-aws s3 cp pio-dist.tar.gz s3://recommendations-io-dist/PROD/common/pio-dist.tar.gz
-aws s3 cp ../engine.tar.gz s3://recommendations-io-dist/PROD/common/engine.tar.gz
+aws s3 cp pio-dist.tar.gz s3://recommendations-dist/PROD/common/pio-dist.tar.gz
+aws s3 cp ../engine.tar.gz s3://recommendations-dist/PROD/common/engine.tar.gz
