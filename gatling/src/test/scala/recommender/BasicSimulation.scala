625d44ea2d5a1300aeb5f88b9cb430fb191c34e6
Max Spencer <max.spencer@guardian.co.uk>
2016-08-24 16:56:03 +0100

    Remove fromOrigin

--- gatling/src/test/scala/recommender/BasicSimulation.scala
+++ gatling/src/test/scala/recommender/BasicSimulation.scala
@@ -3,24 +3,19 @@ package recommender
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
-import io.gatling.core.validation.Validation
 
 
 class BasicSimulation extends Simulation {
-  val routerUrl = "https://mobile.code.dev-guardianapis.com/fronts"
-  val originUrl = "http://engine.mobile.guardianapis.com"
-  //val originUrl = "http://localhost:9000"
+  val baseUrl = "http://engine.mobile.guardianapis.com"
+  //val baseUrl = "http://localhost:9000"
+
   val httpConf = http
-    .baseURL(originUrl) // Here is the root for all relative URLs
+    .baseURL(baseUrl) // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:48.0) Gecko/20100101 Firefox/48.0")
-
-  def fromOrigin(s: String): Session => Validation[String] = { session =>
-    session(s).as[String].replace(routerUrl, originUrl)
-  }
 
   val scn = scenario("Get recommendations") // A scenario is a chain of requests and pauses
     .exec(http("healthcheck")
