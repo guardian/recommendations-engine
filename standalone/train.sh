f44e76bbc2a6c8a25bcc1e741e3b9e6ccddb059e
David Furey <david.furey@guardian.co.uk>
2016-10-05 11:37:25 +0100

    Update training script to use yarn

--- standalone/train.sh
+++ standalone/train.sh
@@ -4,4 +4,4 @@ export HADOOP_USER_NAME=ec2-user
 cd /opt/PredictionIO-0.9.5/
 ./update-yarn-config.sh
 cd /opt/PredictionIO-0.9.5/engines/MyGuardianTestEngine-1
-time ../../bin/pio train -- --master spark://master.spark.recommendations.prod.internal:7077 --driver-memory 1200M --verbose
+time ../../bin/pio train -- --master yarn --deploy-mode client --driver-memory 1200M --verbose
