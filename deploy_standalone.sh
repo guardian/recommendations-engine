11eb2a2a4dfd3120df5bf8b571fede004d6b732d
David Furey <david.furey@guardian.co.uk>
2016-10-05 11:14:16 +0100

    Include all files in standalone package

--- deploy_standalone.sh
+++ deploy_standalone.sh
@@ -3,7 +3,7 @@
 rm -rf target
 mkdir target
 mkdir target/files
-cp standalone/* target/files/
+cp -r standalone/* target/files/
 cd target
 tar -czf package.tar.gz files
 aws s3 cp package.tar.gz s3://prediction-io-dist/mobile-prediction-io/PROD/standalone/package.tar.gz
