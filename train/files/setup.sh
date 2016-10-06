0b0c66eee14fa6e942310f7a0e80042cc081fa92
David Furey <david.furey@guardian.co.uk>
2016-10-06 10:43:18 +0100

    Jq is needed by cluster discovery script

--- train/files/setup.sh
+++ train/files/setup.sh
@@ -4,6 +4,8 @@ instanceid=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone |sed 's/.$//'`
 stagetag=`aws ec2 describe-tags --filters "Name=resource-id,Values=$instanceid" "Name=resource-type,Values=instance" "Name=key,Values=Stage" --region $region | grep -oP "(?<=\"Value\": \")[^\"]+"`
 
+apt-get install -y jq
+
 # Fetch packages
 mkdir -p /root/files/packages
 cd /root/files/packages
