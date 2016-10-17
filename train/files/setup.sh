e2a86982412c88bf1180057689435363454ebad6
David Furey <david.furey@guardian.co.uk>
2016-10-17 13:55:15 +0100

    Install script to create a spark cluster

--- train/files/setup.sh
+++ train/files/setup.sh
@@ -54,6 +54,7 @@ tar zxfC engine.tar.gz PredictionIO-0.9.5/engines
 
 # Install training scripts
 cp /root/files/train.sh PredictionIO-0.9.5/bin/
+cp /root/files/create-cluster.sh PredictionIO-0.9.5/bin/
 cp /root/files/discover-spark-cluster.sh PredictionIO-0.9.5/bin/
 cp /root/files/terminate-clusters.sh PredictionIO-0.9.5/bin/
 cp /root/files/update-yarn-config.sh PredictionIO-0.9.5/bin/
