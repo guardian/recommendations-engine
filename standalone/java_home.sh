4af5ef4dfcb76952a2e9cfef88efa1dd5abbc0ff
David Furey <david.furey@guardian.co.uk>
2016-10-19 10:20:12 +0100

    Standalone has been replaced with train

--- standalone/java_home.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
--- standalone/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export HADOOP_USER_NAME=ec2-user
-cd /opt/PredictionIO-0.9.5/
-./update-yarn-config.sh
-cd /opt/PredictionIO-0.9.5/engines/MyGuardianTestEngine-1
-time ../../bin/pio train -- --master yarn --deploy-mode client --driver-memory 1200M --verbose
