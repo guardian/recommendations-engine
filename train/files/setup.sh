f9c5593878ca2540f674bf7b14bc624a5607febd
Alexandre Dufournet <alex.dufournet@theguardian.com>
2017-04-13 11:37:30 +0100

    use our own hosted version of hbase

--- train/files/setup.sh
+++ train/files/setup.sh
@@ -12,7 +12,7 @@ cd /root/files/packages
 wget -nv https://d8k1yxp8elc6b.cloudfront.net/PredictionIO-0.9.5.tar.gz
 wget -nv http://d3kbcqa49mib13.cloudfront.net/spark-1.5.2-bin-hadoop2.6.tgz
 wget -nv https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz
-wget -nv http://mirrors.ukfast.co.uk/sites/ftp.apache.org/hbase/hbase-1.0.3/hbase-1.0.3-bin.tar.gz
+aws s3 cp s3://recommendations-dist/software/hbase-1.0.3-bin.tar.gz . --region eu-west-1
 
 cd /opt/
 
